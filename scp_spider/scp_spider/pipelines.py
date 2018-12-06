2dbebf264e8ab8e22373f75f937f46c843daa7aa
zhufree <zhufree2013@gmail.com>
2018-12-06 23:58:12 +0800

    scrapy_update

--- scp_spider/scp_spider/pipelines.py
+++ scp_spider/scp_spider/pipelines.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+from scrapy.exceptions import DropItem
+import csv
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -8,4 +9,50 @@
 
 class ScpSpiderPipeline(object):
     def process_item(self, item, spider):
+        raw_list = self.get_scps_from_file('scp_files/scp_list.csv')
+        raw_list.append(dict(item))
+        self.write_to_csv(raw_list, 'scp_files/scp_list.csv')
         return item
+
+    def parse_detail(self, response):
+        detail_dom = response.css('div#page-content')[0]
+        # for category in total_scps_list:
+        #     if category['link'] == link:
+        #         category['not_found'] = "false"
+        #         category['detail'] = detail_dom.html().replace('  ', '').replace('\n', '')
+        print(detail_dom.css('::text')).extract()
+        # a_in_detail = detail_dom.remove('.footer-wikiwalk-nav')('a')
+        # if len(list(a_in_detail.items())) > 30:
+        #     return
+        # for a in a_in_detail.items():
+        #     href = a.attr('href')
+        #     if href.startswith('/') and href not in total_link_list:
+        #         print('new link = ' + href)
+        #         new_link.append(href)
+        #         new_found_link_list.append(href)
+        #         title = a.text()
+        #         new_category = {
+        #             'title': title,
+        #             'link': href,
+        #             'type': 'none'
+        #         }
+        #         new_found_category_list.append(new_category)
+
+    def item_completed(self, results, item, info):
+        return item
+
+    def write_to_csv(self, article_list, file_name):
+        with open(file_name, 'w+', encoding='utf-8', newline='') as f:
+            # 统一header，方便后续合并文件一起上传
+            writer = csv.DictWriter(f, ['link', 'title', 'scp_type', 'detail', 'cn', 'not_found', \
+                'author', 'desc', 'snippet', 'subtext', 'contest_name', 'contest_link', \
+                'created_time', 'month', 'event_type', 'page_code'])
+            writer.writeheader()
+            writer.writerows(article_list)
+    def get_scps_from_file(self, filename):
+        with open(filename, 'r', encoding='utf-8', newline='') as f:
+            # 统一header，方便后续合并文件一起上传
+            reader = csv.DictReader(f)
+            category_list = [dict(order_dict) for order_dict in reader]
+            return category_list
+
--- scp_spider/scp_spider/spiders/scp_spider.py
+++ scp_spider/scp_spider/spiders/scp_spider.py
@@ -1,42 +1,85 @@
 import scrapy
+import csv
+from scp_spider.items import ScpSpiderItem
 
-class mingyan(scrapy.Spider): #需要继承scrapy.Spider类
+class ScpSpider(scrapy.Spider): #需要继承scrapy.Spider类
     
     name = "scp" # 定义蜘蛛名
+    # allowed_domains = 'scp-wiki-cn.wikidot.com'
 
     start_urls = [  #另外一种写法，无需定义start_requests方法
+        # scp系列1-5
         'http://scp-wiki-cn.wikidot.com/scp-series',
-        'http://scp-wiki-cn.wikidot.com/scp-series-1',
         'http://scp-wiki-cn.wikidot.com/scp-series-2',
         'http://scp-wiki-cn.wikidot.com/scp-series-3',
         'http://scp-wiki-cn.wikidot.com/scp-series-4',
     ]
 
 
-    def parse(self, response):
+    spider_header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 
-        '''
-        start_requests已经爬取到页面，那如何提取我们想要的内容呢？那就可以在这个方法里面定义。
-        这里的话，并木有定义，只是简单的把页面做了一个保存，并没有涉及提取我们想要的数据，后面会慢慢说到
-        也就是用xpath、正则、或是css进行相应提取，这个例子就是让你看看scrapy运行的流程：
-        1、定义链接；
-        2、通过链接爬取（下载）页面；
-        3、定义规则，然后提取数据；
-        就是这么个流程，似不似很简单呀？
-        '''
-        uls = response.css('div#page-content ul')    #根据上面的链接提取分页,如：/page/1/，提取到的就是：1
+    
+
+    def parse(self, response):
+        uls = response.css('div#page-content ul')
         for ul in uls[1:-3]:
             for li in ul.css('li'):
                 link = li.css('a::attr(href)').extract_first()
-                new_article = {
-                    'title': li.css('::text'),
-                    'link': link,
-                    'cn': 'false',
-                    'type': 'series'
-                }
-                # link_list.append(link)
-                print(link)
-            # article_list.append(new_article)
-        # with open(filename, 'wb') as f:        #python文件操作，不多说了；
-        #     f.write(response.body)             #刚才下载的页面去哪里了？response.body就代表了刚才下载的页面！
-        # self.log('保存文件: %s' % filename)      # 打个日志
+                new_scp = ScpSpiderItem(
+                    title = '',
+                    link = '',
+                    cn = '',
+                    scp_type= '',
+                    detail='',
+                    not_found = '',
+                    author = '',
+                    desc = '',
+                    snippet = '',
+                    subtext = '',
+                    contest_name = '',
+                    contest_link = '',
+                    created_time = '',
+                    month = '',
+                    event_type = '',
+                    page_code = '',
+                )
+                new_scp['title'] = ''.join(li.css('::text').extract())
+                new_scp['link'] = link
+                new_scp['cn'] = 'false'
+                new_scp['scp_type'] = 'series'
+                
+                detail_request =  scrapy.Request(response.urljoin(link), callback=self.parse_detail, headers = self.spider_header)
+                detail_request.meta['item'] = new_scp
+                yield detail_request
+        #         self.scp_list.append(new_article)
+        # self.write_to_csv(self.scp_list, 'scp_files/scp_list.csv')
+        # self.log('保存文件')
+
+    def parse_detail(self, response):
+        item = response.meta['item']
+        detail_dom = response.css('div#page-content')[0]
+        # for category in total_scps_list:
+        #     if category['link'] == link:
+        #         category['not_found'] = "false"
+        #         category['detail'] = detail_dom.html().replace('  ', '').replace('\n', '')
+        item['detail'] = detail_dom.extract().replace('  ', '').replace('\n', '')
+        yield item
+        # a_in_detail = detail_dom.remove('.footer-wikiwalk-nav')('a')
+        # if len(list(a_in_detail.items())) > 30:
+        #     return
+        # for a in a_in_detail.items():
+        #     href = a.attr('href')
+        #     if href.startswith('/') and href not in total_link_list:
+        #         print('new link = ' + href)
+        #         new_link.append(href)
+        #         new_found_link_list.append(href)
+        #         title = a.text()
+        #         new_category = {
+        #             'title': title,
+        #             'link': href,
+        #             'type': 'none'
+        #         }
+        #         new_found_category_list.append(new_category)
+
+

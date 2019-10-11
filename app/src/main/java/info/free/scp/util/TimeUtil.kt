8d6112c77cc36edfae6850f62fee36b450d6679a
zhufree <zhufree2013@gmail.com>
2019-10-11 19:10:00 +0800

    update:草稿箱日期bug fix

--- app/src/main/java/info/free/scp/util/TimeUtil.kt
+++ app/src/main/java/info/free/scp/util/TimeUtil.kt
@@ -6,7 +6,7 @@ object TimeUtil {
     val calendar = Calendar.getInstance()
     fun timeStampToStr(timeStamp: Long): String {
         calendar.timeInMillis = timeStamp
-        return "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH)}/${calendar.get(Calendar.DAY_OF_MONTH)}" +
+        return "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.DAY_OF_MONTH)}" +
                 " ${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}:${calendar.get(Calendar.SECOND)}"
     }
 }

ab516a092e6c3c79bfd45be422de68654192038b
zhufree <zhufree2013@gmail.com>
2021-02-09 10:03:35 +0800

    random list ui

---
+++ app/src/main/java/info/free/scp/view/random/RandomActivity.kt
@@ -0,0 +1,12 @@
+package info.free.scp.view.random
+
+import android.os.Bundle
+import info.free.scp.R
+import info.free.scp.view.base.BaseActivity
+
+class RandomActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random)
+    }
+}
---
+++ app/src/main/java/info/free/scp/view/random/RandomAdapter.kt
@@ -0,0 +1,62 @@
+package info.free.scp.view.random
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import info.free.scp.bean.ScpModel
+import info.free.scp.databinding.ItemRandomBinding
+import info.free.scp.view.detail.DetailActivity
+
+class RandomAdapter : ListAdapter<ScpModel, RandomAdapter.RandomHolder>(RandomDiffCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RandomHolder {
+        return RandomHolder((ItemRandomBinding.inflate(LayoutInflater.from(parent.context), parent,
+                false)))
+    }
+
+    override fun onBindViewHolder(holder: RandomHolder, position: Int) {
+        val scp = getItem(position)
+        holder.apply {
+            bind(createOnClickListener(scp), scp)
+        }
+    }
+
+    private fun createOnClickListener(scp: ScpModel): View.OnClickListener {
+        return View.OnClickListener {
+            val intent = Intent()
+            // TODO
+            intent.putExtra("link", scp.link)
+            intent.putExtra("forceOnline", true)
+            intent.setClass(it.context, DetailActivity::class.java)
+            (it.context as Activity).startActivity(intent)
+        }
+    }
+
+    class RandomHolder(private val bd: ItemRandomBinding) : RecyclerView.ViewHolder(bd.root) {
+
+        fun bind(listener: View.OnClickListener, scp: ScpModel) {
+            bd.apply {
+                title = scp.title
+                clickListener = listener
+                executePendingBindings()
+            }
+        }
+    }
+
+    class RandomDiffCallback : DiffUtil.ItemCallback<ScpModel>() {
+
+        override fun areItemsTheSame(oldItem: ScpModel, newItem: ScpModel): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ScpModel, newItem: ScpModel): Boolean {
+            return oldItem.link == newItem.link
+        }
+    }
+}
